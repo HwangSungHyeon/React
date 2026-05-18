d55e5eb690bc8fc884ae670b9b83bf0e44a47775
HwangSungHyeon <asyoung316@gmail.com>
2026-05-18 17:25:41 +0900

    26.05.18

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import StudentPage from './page/StudentPage'
+function App() {
+
+  return (
+    <>
+      <h1>학생 학습 분석 리포트 시스템</h1>
+      <StudentPage />
+    </>
+  )
+}
+
+export default App
