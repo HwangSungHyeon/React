72b4802621045302190407f9ee29a78a8d9313f8
HwangSungHyeon <asyoung316@gmail.com>
2026-05-21 16:42:24 +0900

    26.05.21

--- frontend/src/page/StudentAnalysisPage.jsx
+++ frontend/src/page/StudentAnalysisPage.jsx
@@ -1,63 +1,80 @@
-
 import { useEffect, useState } from "react";
 
-function StudentAnalysisPage(){
-    const[dangerStd, SetDangerStd] = useState([]);
-    const[excellentStd, SetExcellentStd] = useState([]);
+function StudentAnalysisPage() {
 
-    const getAnalysis = async()=> {
-        const respose = await fetch("http://localhost:8000/analysis/student-status");
-        const data = await respose.json();
+    const [dangerStd, SetDangerStd] = useState([]);
+    const [excellentStd, SetExcellentStd] = useState([]);
+
+    const getAnalysis = async () => {
+        const response = await fetch("http://localhost:8000/analysis/student-status");
+
+        const data = await response.json();
+
         SetDangerStd(data.danger_std);
         SetExcellentStd(data.excellent_std);
     };
 
-    useEffect( () =>{
+    useEffect(() => {
         getAnalysis();
     }, []);
 
-    return(
+    return (
         <div>
-            <session>
+
+            <section>
                 <h2>관리 학생</h2>
+
                 <table>
-                    <tr>
-                        <th>이름</th>
-                        <th>평균점수</th>
-                        <th>출석률</th>
-                        <th>상태</th>
-                    </tr>
-                    {dangerStd.map((std) => (
-                    <tr>
-                        <td>{std.name}</td>
-                        <td>{std.avg_score}</td>
-                        <td>{std.attend_rate}</td>
-                        <td>{std.status}</td>
-                    </tr>
-                    ))}
+                    <thead>
+                        <tr>
+                            <th>이름</th>
+                            <th>평균점수</th>
+                            <th>출석률</th>
+                            <th>상태</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {dangerStd.map((std, index) => (
+                            <tr key={index}>
+                                <td>{std.name}</td>
+                                <td>{std.avg_score}</td>
+                                <td>{std.attend_rate}</td>
+                                <td>{std.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
-            </session>
-            <session>
+            </section>
+
+            <section>
                 <h2>우수 학생</h2>
+
                 <table>
-                    <tr>
-                        <th>이름</th>
-                        <th>평균점수</th>
-                        <th>출석률</th>
-                        <th>상태</th>
-                    </tr>
-                    {excellentStd.map((std) => (
-                    <tr>
-                        <td>{std.name}</td>
-                        <td>{std.avg_score}</td>
-                        <td>{std.attend_rate}</td>
-                        <td>{std.status}</td>
-                    </tr>
-                    ))}
+                    <thead>
+                        <tr>
+                            <th>이름</th>
+                            <th>평균점수</th>
+                            <th>출석률</th>
+                            <th>상태</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {excellentStd.map((std, index) => (
+                            <tr key={index}>
+                                <td>{std.name}</td>
+                                <td>{std.avg_score}</td>
+                                <td>{std.attend_rate}</td>
+                                <td>{std.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
-            </session>
+            </section>
+
         </div>
-    )
+    );
 }
 
-export default StudentAnalysisPage
+export default StudentAnalysisPage;
